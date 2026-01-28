761db57a86c3099888dac5e8e698a9fb19324522
Hassan Ali Mehdi <su92-bscsm-f23-211@superior.edu.pk>
2026-01-28 17:50:18 +0500

    Sitemap Sorted

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://codantrix.com'
+    const baseUrl = 'https://labs.codantrix.com'
 
     const routes = [
         '',
